519d3166b975e39989c30258f71169a75248a552
Ngan Hoang <ngan@voxmedia.com>
2016-03-25 13:55:57 -0400

    adding my existing site

---
+++ contactthanks.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <html lang="en-US">
+  <head profile="http://ngansense.com">
+  <link rel="icon" 
+      type="image/png" 
+      href="/img/nh-favicon.png">
+  <link href="/css/normalize.css"rel="stylesheet" type="text/css" />
+  <link href="/css/style.css"rel="stylesheet" type="text/css" />
+  <script type="text/javascript" src="//use.typekit.net/tyt7agr.js"></script>
+  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+  <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+</head>
+<body>
+
+  <div class="menu">
+    <nav class="links">
+      <ul>
+        <li><a href="#intro">About</a></li>
+        <li><a href="#work">Work</a></li>
+        <li><a href="index.html"><img src="/img/logo.png" class="logo" alt"ngansense logo"></a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#">Resume</a></li>
+      </ul>
+    </nav>
+  </div>
+  
+  <div id="intro">
+    <div class="about">
+      <h3>YAAAS! Your message has been sent!</h3>
+    </div>
+  </div>
+		
+  <center>
+	<h2><a href="index.html">Go Back</a></h2>
+	</center>
+  
+	</div>
+	
+	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+	</script>
+	<script type="text/javascript">
+	_uacct = "UA-68528-29";
+	urchinTracker();
+	</script>
+
+</body>
+
+</html>
